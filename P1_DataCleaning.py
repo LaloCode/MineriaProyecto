3d062c8541bee3613d111bf49b29ff6534e3250d
LaloCode <lalo_pichu22@hotmail.com>
2022-11-13 21:11:54 -0600

    Filter less data

--- P1_DataCleaning.py
+++ P1_DataCleaning.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 data = pd.read_csv("data/top10s.csv", encoding='cp1252', index_col=0)
 
-data.drop(columns=['bpm', 'nrgy', 'dnce', 'dB', 'live', 'val', 'dur', 'acous', 'spch', 'pop'], inplace=True)
+data.drop(columns=['live', 'val', 'acous', 'spch'], inplace=True)
 
-data.rename(columns={'top genre': 'genre'}, inplace=True)
+data.rename(columns={'top genre': 'genre', 'nrgy': 'energy', 'dnce': 'dance', 'dur': 'duration', 'pop': 'popularity'}, inplace=True)
 
 #data['genre'].apply(filterGenres) 
 
