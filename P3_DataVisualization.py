b5d7158b9af71df5a3e72cd21b7364dae7a76c0e
LaloCode <lalo_pichu22@hotmail.com>
2022-11-14 01:10:30 -0600

    Changed variable to sort values by

--- P3_DataVisualization.py
+++ P3_DataVisualization.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('data/top10s_clean.csv', index_col=0).sort_values(by='valence', axis=0, ascending=False)
+data = pd.read_csv('data/top10s_clean.csv', index_col=0).sort_values(by='dance', axis=0, ascending=False)
 
 top100 = data.head(100)
 bot100 = data.tail(100)
 
-plt.scatter(top100.bpm, top100.valence, color='green', label='100 canciones con mas energia positiva')
-plt.scatter(bot100.bpm, bot100.valence, color='red', label='100 canciones con mas energia negativa')
+plt.scatter(top100.bpm, top100.valence, color='green', label='100 canciones mas bailables')
+plt.scatter(bot100.bpm, bot100.valence, color='red', label='100 canciones menos bailables')
 plt.legend()
 plt.title('Relacion del bpm con la energia de la cancion')
 plt.xlabel('BPM')
